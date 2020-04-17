b1a33809e8014504d316891aedfde3be1dff940b
Jukka Aho <ahojukka5@gmail.com>
2020-04-17 21:11:30 +0700

    Update Main.java
    Print some initial Board.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sudoku solver");
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("| JaSSOO - Java Sudoku Solver with Object-Oriented approach |");
+        System.out.println("+-----------------------------------------------------------+");
+        int[][] data = new int[][]{
+            {0, 0, 0, 0, 0, 0, 2, 0, 0},
+            {0, 8, 0, 0, 0, 7, 0, 9, 0},
+            {6, 0, 2, 0, 0, 0, 5, 0, 0},
+            {0, 7, 0, 0, 6, 0, 0, 0, 0},
+            {0, 0, 0, 9, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 2, 0, 0, 4, 0},
+            {0, 0, 5, 0, 0, 0, 6, 0, 3},
+            {0, 9, 0, 4, 0, 0, 0, 7, 0},
+            {0, 0, 6, 0, 0, 0, 0, 0, 0}};
+        Board board = new Board(data);
+        System.out.println(board);
     }
 }
