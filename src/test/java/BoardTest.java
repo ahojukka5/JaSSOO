7ffabfd9f161c803b730f9146cea821f4887fea9
Jukka Aho <ahojukka5@gmail.com>
2020-04-18 00:14:17 +0700

    Add firsts tests to Board.java

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @Test public void testGetData() {
+        int[][] data = new int[][]{
+            {0, 0, 0, 0, 0, 0, 2, 0, 0},
+            {0, 8, 0, 0, 0, 7, 0, 9, 0},
+            {6, 0, 2, 0, 0, 0, 5, 0, 0},
+            {0, 7, 0, 0, 6, 0, 0, 0, 0},
+            {0, 0, 0, 9, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 2, 0, 0, 4, 0},
+            {0, 0, 5, 0, 0, 0, 6, 0, 3},
+            {0, 9, 0, 4, 0, 0, 0, 7, 0},
+            {0, 0, 6, 0, 0, 0, 0, 0, 0}};
+        Board board = new Board(data);
+        assertArrayEquals(data, board.getData());
+    }
+
+    @Test public void testGetIndex() {
+        int[][] data = new int[][]{
+            {0, 0, 0, 0, 0, 0, 2, 0, 0},
+            {0, 8, 0, 0, 0, 7, 0, 9, 0},
+            {6, 0, 2, 0, 0, 0, 5, 0, 0},
+            {0, 7, 0, 0, 6, 0, 0, 0, 0},
+            {0, 0, 0, 9, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 2, 0, 0, 4, 0},
+            {0, 0, 5, 0, 0, 0, 6, 0, 3},
+            {0, 9, 0, 4, 0, 0, 0, 7, 0},
+            {0, 0, 6, 0, 0, 0, 0, 0, 0}};
+        Board board = new Board(data);
+        assertEquals(6, board.getIndex(2,0));
+    }
+
+}
+
