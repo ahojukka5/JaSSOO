e7578d10f812e496a8ae077b12130b8482f477c4
Jukka Aho <ahojukka5@gmail.com>
2020-05-12 21:03:59 +0700

    Add test for hasInRow()

--- src/test/java/sudoku/BoardTest.java
+++ src/test/java/sudoku/BoardTest.java
@@ -2,6 +2,8 @@ package sudoku;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -61,5 +63,11 @@ public class BoardTest {
   @Test
   public void testGetIndex() {
     assertEquals(6, getBoard().getIndex(2, 0));
+  }
+
+  @Test
+  public void testHasInRow() {
+    assertTrue(getBoard().hasInRow(0, 2));
+    assertFalse(getBoard().hasInRow(1, 2));
   }
 }
